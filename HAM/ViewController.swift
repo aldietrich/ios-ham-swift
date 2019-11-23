912a2927567276c03f0b25ebd791a8b99b722a3e
Andrew <andrewldietrich@gmail.com>
2019-11-23 10:37:25 -0600

    Better version -- no pauses between hams

--- HAM/ViewController.swift
+++ HAM/ViewController.swift
@@ -9,6 +9,89 @@
 import UIKit
 import AVFoundation
 
+class GSAudio: NSObject, AVAudioPlayerDelegate {
+
+    static let sharedInstance = GSAudio()
+
+    private override init() { }
+
+    var players: [URL: AVAudioPlayer] = [:]
+    var duplicatePlayers: [AVAudioPlayer] = []
+
+    func playSound(soundFileName: String) {
+
+        guard let bundle = Bundle.main.path(forResource: soundFileName, ofType: "mp3") else { return }
+        let soundFileNameURL = URL(fileURLWithPath: bundle)
+
+        if let player = players[soundFileNameURL] { //player for sound has been found
+
+            if !player.isPlaying { //player is not in use, so use that one
+                player.prepareToPlay()
+                player.play()
+            } else { // player is in use, create a new, duplicate, player and use that instead
+
+                do {
+                    let duplicatePlayer = try AVAudioPlayer(contentsOf: soundFileNameURL)
+
+                    duplicatePlayer.delegate = self
+                    //assign delegate for duplicatePlayer so delegate can remove the duplicate once it's stopped playing
+
+                    duplicatePlayers.append(duplicatePlayer)
+                    //add duplicate to array so it doesn't get removed from memory before finishing
+
+                    duplicatePlayer.prepareToPlay()
+                    duplicatePlayer.play()
+                } catch let error {
+                    print(error.localizedDescription)
+                }
+
+            }
+        } else { //player has not been found, create a new player with the URL if possible
+            do {
+                let player = try AVAudioPlayer(contentsOf: soundFileNameURL)
+                players[soundFileNameURL] = player
+                player.prepareToPlay()
+                player.play()
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+
+
+    func playSounds(soundFileNames: [String]) {
+        for soundFileName in soundFileNames {
+            playSound(soundFileName: soundFileName)
+        }
+    }
+
+    func playSounds(soundFileNames: String...) {
+        for soundFileName in soundFileNames {
+            playSound(soundFileName: soundFileName)
+        }
+    }
+
+    func playSounds(soundFileNames: [String], withDelay: Double) { //withDelay is in seconds
+        for (index, soundFileName) in soundFileNames.enumerated() {
+            let delay = withDelay * Double(index)
+            let _ = Timer.scheduledTimer(timeInterval: delay, target: self, selector: #selector(playSoundNotification(_:)), userInfo: ["fileName": soundFileName], repeats: false)
+        }
+    }
+
+    @objc func playSoundNotification(_ notification: NSNotification) {
+        if let soundFileName = notification.userInfo?["fileName"] as? String {
+            playSound(soundFileName: soundFileName)
+        }
+    }
+
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        if let index = duplicatePlayers.firstIndex(of: player) {
+            duplicatePlayers.remove(at: index)
+        }
+    }
+
+}
+
 class ViewController: UIViewController {
     @IBOutlet var handleGesture: UILongPressGestureRecognizer!
     
@@ -17,23 +100,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let sound = Bundle.main.path(forResource: "HAM", ofType: "mp3")
-        
-        do {
-            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
-        } catch {
-            print(error)
-        }
-        //Set the audio session to playback to ignore mute switch on device
-        do {
-            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
-        } catch {
-            //Didn't work
-        }
+
     }
 
     @IBAction func play(_ sender: Any) {
-        audioPlayer.play()
+        GSAudio.sharedInstance.playSound(soundFileName: "HAM")
     }
     @IBAction func handleGesture(_ sender: UILongPressGestureRecognizer) {
         if sender.state == .began
